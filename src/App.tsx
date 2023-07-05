8b32979e71580f162b8c58e8daca229dcac42908
sumire <violeta.jin.726@gmail.com>
2023-07-05 12:32:02 +0900

    Login作成中

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.scss";
 import Sidebar from "./compornents/sidebar/Sidebar";
 import Chat from "./compornents/chat/Chat";
+import userEvent from "@testing-library/user-event";
+import userSlice from "./features/userSlice";
 
 function App() {
+  // const user = useSelector((state) => state.user.user);
+  const user = null;
+
   return (
     <div className="App">
-      {/* sidebar*/}
-      <Sidebar />
+      {user ? (
+        <>
+          <Sidebar />
 
-      {/* chat */}
-      <Chat />
+          <Chat />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
